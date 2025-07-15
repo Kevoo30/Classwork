1e8091065a2bb6b5d5582d7c1b194b3d70c210ce
Kevoo30 <muokikelvin55@gmail.com>
2025-07-15 09:55:58 +0300

    commit

--- search_algorithm.py
+++ search_algorithm.py
@@ -1,29 +1,20 @@
-numbers = [11, 22, 33, 44, 55, 66, 77, 88, 99]
-key = 77
-start = 0
-
-end = len(numbers) - 1
-found = False
-
-
-for i in range(len(numbers)):
-    if numbers[i] == key:
-        print("Found")
-        break
-    else:
-        print("Not Found")
-
-
+arr = [11,22,33,44,55,66,77,88,99]
+key: int = 22
+start: int= 0
+end = len(arr)
+n = len(arr)-1
+found: bool = False
 while start <= end:
-    mid = (start + end) // 2
-    if numbers[mid] == key:
-        print("Found element at pos:", mid)
+    mid = (start+end)//2
+
+    if arr[mid] == key:
+        print("Found element at index ",mid)
         found = True
         break
-    elif key < numbers[mid]:
-        end = mid - 1
-    else:
-        start = mid + 1
 
+    elif key<arr[mid]:
+        end = mid-1
+    else:
+        start=mid+1
 if not found:
-    print(f"{key} not found in the list")
+    print("Element not found")
